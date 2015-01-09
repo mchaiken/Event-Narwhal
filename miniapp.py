d0dc4feccd2b6a1d31eff88123920f692f4d5667
Jenny Baran <jenny.baran@gmail.com>
2015-01-09 11:15:51 -0500

    weird merge conflicts, hopefully resolved

--- miniapp.py
+++ miniapp.py
@@ -12,5 +12,10 @@ def test():
     return render_template("test.html")
 
 if __name__ == "__main__":
+<<<<<<< Updated upstream
     #app.run(host="149.89.150.1", port=1639)
     app.run()
+=======
+    app.run()
+#app.run(host="149.89.150.1", port=1639)
+>>>>>>> Stashed changes
