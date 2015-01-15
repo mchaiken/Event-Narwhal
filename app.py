c846d927c6652aeda759396b67905320c0c8368b
mchaiken <mirandachaiken@gmail.com>
2015-01-15 11:48:40 -0500

    debugging the branch

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from pymongo import Connection
-
+import yummly
 #mongo setup
 conn = Connection()
 db = conn["even_narwhal"]
 
+#flask setup
+app = Flask(__name__)
 
 
 def validated(user_id):
@@ -24,15 +26,15 @@ def logout():
     session.pop('user',None)
     return render_template("logout.html")
 
-@app.route("/foodresults", method=["GET","POST"])
+@app.route("/food", methods=["GET","POST"])
 def foodresults():
     if request.method=="GET":
         attribute= request.args.get("mymenu")
         search = request.args.get("search")
-            if attribute== "ingredients":   
+        if attribute== "ingredients":
                 search= search.split(",")
     #how do I seperate the request method from the drop down from the search bar?
-        results=yummly.getResults(attribute,search)
+        results=yummly.getResults(attribute,"onions")
     return render_temple("foodresults.html", attribute=attribute, results= results)    
 
 
--- yummly.py
+++ yummly.py
@@ -8,6 +8,7 @@ we will need to have a feature that will allow for specific searches
 Supported Holidays
 Christmas, Summer, Thanksgiving, New Year, Super Bowl / Game Day, Halloween, Hanukkah, 4th of July
 '''
+import urllib2, json
 
 
 
@@ -17,23 +18,22 @@ def getResults(attribute, search):
     #attribute= if it is a holiday, cusine, or ingredient
     #search= their actual input
     #need to take into account if their search is not a real holiday
+    url=""
     if attribute == "holiday":
         url ="http://api.yummly.com/v1/api/recipes?maxResult=100&_app_id=dd74dd78&_app_key=992e5769b7da1040ad87d47328a4182e&q=&allowedHoliday[]=holiday^holiday-"+ search.lower()
-    if attribute == "cuisine":
+    elif attribute == "cuisine":
         url ="http://api.yummly.com/v1/api/recipes?maxResult=100&_app_id=dd74dd78&_app_key=992e5769b7da1040ad87d47328a4182e&q=&allowedCusine[]=cuisine^cuisine-"+ search.lower()
-    if attribute == "ingredient":
+    else:
          url=""" http://api.yummly.com/v1/api/recipes?maxResult=100&_app_id=dd74dd78&_app_key=992e5769b7da1040ad87d47328a4182e"""
          for ingredient in search:
              if ingredient[0]==" ":
                  ingredient= ingredient[1:]
                  url+="&allowedIngredient[]="+ingredient.lower()
-    #print url
     request = urllib2.urlopen(url)
     result = request.read()
     #print url
     return json.loads(result)
 
-m,mn,n 
 
 def getRecipe(id):
     url="http://api.yummly.com/v1/api/recipe/"+id+"?_app_id=dd74dd78&_app_key=992e5769b7da1040ad87d47328a4182e"
