ee775eb0226916135fdce9dea27cf49f0fcf8443
Leon Frajmund <leonfrajmund@gmail.com>
2015-01-21 21:20:00 -0500

    got FB methods to work with the access token

--- app.py
+++ app.py
@@ -4,17 +4,19 @@ import database_actions
 #pip install facebook-sdk
 import facebook
 import json
+import FB
 
 
 #fb setup
-FBAppID = "1003039823056323"
-FBAppSecret = "15fb5524a4926a2be5e881177c135584"
-FBAccessToken = ""
+FBAppID = "1556299804628530"
+FBAppSecret = "caf5c0f2cd36a15c61949d9991170d0d"
+FBAccessToken = "TBD"
 #http://stackoverflow.com/questions/10693630/how-to-pass-a-boolean-from-javascript-to-python use this
 #for the js to python transfer
 graph = facebook.GraphAPI(FBAccessToken)
 def getEvents():
         return graph.get_connections(id = "me", connection_name = "events")
+
 
    
 #mongo setup
@@ -26,56 +28,54 @@ db = conn["event_narwhal"]
 app = Flask(__name__)
 
 def validated(user_id):
-    sessions[user]= db.users.find("user_id:"+user_id)
-
-
+        sessions[user]= db.users.find("user_id:"+user_id)
+        
 @app.route("/", methods = ["GET", "POST"])
 @app.route("/home", methods = ["GET", "POST"])
 def home():
-    if 'user' not in session:
-        cookie = facebook.get_user_from_cookie(request.cookies, FBAppID, FBAppSecret)
-        #print cookie["access_token"]
-        if cookie != None:
-            FBAccessToken = cookie["access_token"]
-            session["user"]= cookie["access token"];
-
-            #print getEvents()
-        return render_template("home.html")
-    return render_template("my_events.html", events=database_actions.get_events(123456789))
+        if 'user' not in session:
+                cookie = facebook.get_user_from_cookie(request.cookies, FBAppID, FBAppSecret)
+                print cookie
+                if cookie != None:
+                        global FBAccessToken
+                        FBAccessToken = cookie["access_token"]
+                        session["user"]= cookie["access_token"];
+                return render_template("home.html")
+        return render_template("my_events.html", events=database_actions.get_events(123456789))
 
 
 @app.route("/new")
 def new_event():
-    if 'user' not in session:
-       return redirect('/')
-    #if request_method == "POST":
-    #   database_actions.add_event(name=request.form["name"]) #this isn't done, but just a placeholder
-    return render_template('settings.html',facebook_events=["event1","event2"],events=database_actions.get_events(session["user"]))
+        if 'user' not in session:
+                return redirect('/')
+        #if request_method == "POST":
+        #   database_actions.add_event(name=request.form["name"]) #this isn't done, but just a placeholder
+        return render_template('settings.html',facebook_events=["event1","event2"],events=database_actions.get_events(session["user"]))
 
 @app.route("/event/<event_index>")
 def event(event_index):
-    if 'user' not in session:
-       return redirect("/")
-    return render_template("event.html",event=database_actions.get_event(session["user"],event_index),events=database_actions.get_events(123456789))
+        if 'user' not in session:
+                return redirect("/")
+        return render_template("event.html",event=database_actions.get_event(session["user"],event_index),events=database_actions.get_events(123456789))
 
 #logout button on other pages will redirect to this
 @app.route("/logout")
 def logout():
-    #log user out
-    #page will have button to return to login page
-    session.pop('user',None)
-    return render_template("logout.html")
+        #log user out
+        #page will have button to return to login page
+        session.pop('user',None)
+        return render_template("logout.html")
 #logout button on other pages will redirect to this
 
 @app.route("/login")
 def login():
-    #this is temporary until we have fb working
-    #page will have button to return to login page
-    session['user']=123456789
-    return redirect("/")
+        #this is temporary until we have fb working
+        #page will have button to return to login page
+        session['user']=123456789
+        return redirect("/")
 
 
 if __name__ == "__main__":
-    app.debug = True
-    app.secret_key=open("secret_key.txt").read()
-    app.run(host="149.89.150.1")
+        app.debug = True
+        app.secret_key=open("secret_key.txt").read()
+        app.run(host="149.89.150.1")
