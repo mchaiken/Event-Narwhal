bdda394c40c5c61736b2efc8f22e141f4291beb4
Leon Frajmund <leonfrajmund@gmail.com>
2015-01-22 11:21:19 -0500

    just saving a thing

--- FB.py
+++ FB.py
@@ -5,11 +5,15 @@ def Graph(token):
 def getEvents(token):
         graph = Graph(token)
         return graph.get_connections("me", "events")
+
+def
+
 def getID(token):
         graph = Graph(token)
         return graph.get_object("me")["id"]
 
-
+def getGuests(token, event):
+        
 #to make:
 #getGuests  
 #optional: getAttending   getNotAttending   getMaybe   getUnknown 
