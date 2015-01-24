023a611208f19841fc73c423750b64c50cf65193
Jenny Baran <jenny.baran@gmail.com>
2015-01-24 10:38:58 -0500

    cleaning up code & files

--- yummly.py
+++ yummly.py
@@ -1,4 +1,3 @@
-
 '''
 <form action="/wherewearedirecting" method ="GET">
 <input type="text" name="search">
@@ -8,38 +7,34 @@ we will need to have a feature that will allow for specific searches
 Supported Holidays
 Christmas, Summer, Thanksgiving, New Year, Super Bowl / Game Day, Halloween, Hanukkah, 4th of July
 '''
+
 import urllib2, json
 
-
-
-
-
-def getResults(attribute, search):
-    #attribute= if it is a holiday, cusine, or ingredient
-    #search= their actual input
+def getResults( attribute, search ):
+    #attribute = if it is a holiday, cusine, or ingredient
+    #search = their actual input
     #need to take into account if their search is not a real holiday
-    url=""
+    url = ""
     if attribute == "holiday":
-        url ="http://api.yummly.com/v1/api/recipes?maxResult=100&_app_id=dd74dd78&_app_key=992e5769b7da1040ad87d47328a4182e&q=&allowedHoliday[]=holiday^holiday-"+ search.lower()
+        url = "http://api.yummly.com/v1/api/recipes?maxResult=100&_app_id=dd74dd78&_app_key=992e5769b7da1040ad87d47328a4182e&q=&allowedHoliday[]=holiday^holiday-" + search.lower()
     elif attribute == "cuisine":
-        url ="http://api.yummly.com/v1/api/recipes?maxResult=100&_app_id=dd74dd78&_app_key=992e5769b7da1040ad87d47328a4182e&q=&allowedCusine[]=cuisine^cuisine-"+ search.lower()
+        url = "http://api.yummly.com/v1/api/recipes?maxResult=100&_app_id=dd74dd78&_app_key=992e5769b7da1040ad87d47328a4182e&q=&allowedCusine[]=cuisine^cuisine-" + search.lower()
     else:
-         url=""" http://api.yummly.com/v1/api/recipes?maxResult=100&_app_id=dd74dd78&_app_key=992e5769b7da1040ad87d47328a4182e"""
+         url = "http://api.yummly.com/v1/api/recipes?maxResult=100&_app_id=dd74dd78&_app_key=992e5769b7da1040ad87d47328a4182e"
          for ingredient in search:
-             if ingredient[0]==" ":
-                 ingredient= ingredient[1:]
-                 url+="&allowedIngredient[]="+ingredient.lower()
-    request = urllib2.urlopen(url)
+             if ingredient[0] == " ":
+                 ingredient = ingredient[1:]
+                 url += "&allowedIngredient[]=" + ingredient.lower()
+    request = urllib2.urlopen( url )
     result = request.read()
     #print url
-    return json.loads(result)
+    return json.loads( result )
 
 
-def getRecipe(id):
-    url="http://api.yummly.com/v1/api/recipe/"+id+"?_app_id=dd74dd78&_app_key=992e5769b7da1040ad87d47328a4182e"
+def getRecipe( id ):
+    url = "http://api.yummly.com/v1/api/recipe/" + id + "?_app_id=dd74dd78&_app_key=992e5769b7da1040ad87d47328a4182e"
     #print url
-    request = urllib2.urlopen(url)
+    request = urllib2.urlopen( url )
     result = request.read()
     #print url
-    return json.loads(result)
-
+    return json.loads( result )
