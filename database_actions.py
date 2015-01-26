76a2f8663c2b6a086a545b9504206ae206d5ce37
Jenny Baran <jenny.baran@gmail.com>
2015-01-25 19:31:30 -0500

    working on rsvps

--- database_actions.py
+++ database_actions.py
@@ -4,10 +4,12 @@ from pymongo import Connection
 conn = Connection()
 db = conn["event_narwhal"]
 
+
 #inserts the user's fb id and username into the collection
 def register_user(user_name,fb_id):
     db.users.insert({ "_id":fb_id, "name":user_name , "events":[] })
     return True
+
 
 #True if user has registered through fb, False otherwise
 def isRegistered(fb_id):
@@ -19,9 +21,11 @@ def isRegistered(fb_id):
         return True
     return False
 
+
 #returns dictionary of user's info including fb_id username and events
 def login_user(fb_id):
     return db.users.find_one({"_id":fb_id})
+
 
 #create a new event
 def add_event(id,name,theme,fb, description, date, location, attending, declined, maybe, not_responded):
@@ -37,19 +41,23 @@ def remove_event(id,index):
     print db.users.find_one({"_id":id}).get("name")
     return True
 
+
 #get a user's events
 def get_events(id):
     return db.users.find_one({"_id":id}).get("events")
+
 
 #get those attending a given event with the user's ID and the Index of the event
 def get_event(id,index):
     events=get_events(id)
     return events[int(index.encode("utf8"))]
 
+
 def update_8tracks(id,index,link):
     events=get_events(id)
     events[index]["8tracks"]=link
     db.users.update({"_id":id},{"$set":{"events":events}})
+
 
 def update_yummly(id,index,link):
     events=get_events(id)
@@ -61,9 +69,7 @@ def update_yummly(id,index,link):
 def get_attending(id,index):
     events=get_events(id)
     return events[int(index.encode("utf8"))]["attending"]
-                  
-                  
-                  
+
 
 #not sure if this should be in the yummly section or not so I will comment it out
 '''
